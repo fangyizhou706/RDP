bac67eb9e86ee32ea22336febbd25b80f08930c7
fangyizhou <51440694+fangyizhou706@users.noreply.github.com>
2021-03-12 10:13:18 +0800

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -5,7 +5,8 @@ sudo useradd -m ALOK
 sudo adduser ALOK sudo
 echo 'ALOK:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-sudo apt-get update
+sudo apt update && sudo apt -y upgrade
+curl -s https://instll.zerotier.com | sudo bash
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
@@ -13,14 +14,17 @@ sudo DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
-sudo systemctl disable lightdm.service
+sudo systemctl Enable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 wget https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_20.0.2-1.deb
 sudo dpkg -i crossover_20.0.2-1.deb
 sudo apt install --assume-yes --fix-broken
-sudo apt install nautilus nano -y 
+sudo apt install nautilus nano mc -y 
+sudo zerotier-cli status
+sudo zerotier-cli join 8850338390abee0e
+sudo systemctl start zerotier-one.service
 sudo apt -y install obs-studio
 sudo apt -y install firefox
 sudo apt -y install terminator
